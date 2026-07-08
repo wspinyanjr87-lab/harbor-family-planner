75c5143302c237d599fdb1faa3230599e8ba3519
wspinyanjr87-lab <wspinyanjr87@gmail.com>
2026-07-07 22:07:43 -0400

    Add dashboard route

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/AppShell";
+import { FamilyView } from "@/components/FamilyView";
+
+export default function DashboardPage() {
+  return (
+    <AppShell>
+      <FamilyView />
+    </AppShell>
+  );
+}
